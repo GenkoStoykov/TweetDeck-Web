5ce5788ee6738d3ba99f1212e60065f49357e443
GenkoStoykov <G.Stoykov@yandex.com>
2017-06-16 15:58:32 -0700

    second commit

--- crawler/config.py
+++ crawler/config.py
@@ -4,7 +4,8 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "XxXxXxxXXXxxxxXXXxXX"
-consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
-access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
-access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
+consumer_key = "gbbO6thCfWxKjp99v7CWGxiU5"
+consumer_secret = "tw3Qha7gV4a3xew2euXj6UWYO3hiMlpLWuudYR3w5AegLgp2bQ"
+access_key = "316545541-KuAqgwnwoG2U0hteVVogQCOCozLkhIVIcChnZ19O"
+access_secret = "gtfBT1nhbsh4l1IoQ9FubV9Uz6t0LItiyRC2NywW10zV8"
+
